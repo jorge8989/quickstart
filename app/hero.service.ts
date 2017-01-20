5a82dda89d495a95076d65045015c1eb3a6d2952
jorge <jorgearteaga8936@gmail.com>
2017-01-19 22:15:49 -0400

    completed basi CRUD

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -33,8 +33,25 @@ export class HeroService {
       .then(() => hero)
       .catch(this.handleError);
   }
+
+  create(name: string): Promise<Hero> {
+  return this.http
+    .post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
+    .toPromise()
+    .then(res => res.json().data)
+    .catch(this.handleError);
+  }
+
   private handleError(error: any): Promise<any> {
     console.error('An error occurred', error); // for demo purposes only
     return Promise.reject(error.message || error);
+  }
+
+  delete(id: number): Promise<void> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete(url, {headers: this.headers})
+      .toPromise()
+      .then(() => null)
+      .catch(this.handleError);
   }
 }
